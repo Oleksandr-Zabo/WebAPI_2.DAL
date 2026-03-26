7a20368cb890afd3107c30cc3afd6c6bf59f3432
Oleksandr <zoleksandr06@gmail.com>
2026-03-26 20:03:49 +0200

    add docker and readme

--- obj/Release/net10.0/WebAPI_2.DAL.AssemblyInfo.cs
+++ obj/Release/net10.0/WebAPI_2.DAL.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI_2.DAL")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+378e5adfdc204dbdb218c512d4bad4d3bf1bec58")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI_2.DAL")]
