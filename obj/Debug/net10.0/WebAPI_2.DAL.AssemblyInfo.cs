d3de43171fa44bcfd2a76044aa7872016d5dd376
Oleksandr <zoleksandr06@gmail.com>
2026-03-30 16:25:55 +0300

    fix paths for docker

--- obj/Debug/net10.0/WebAPI_2.DAL.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebAPI_2.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI_2.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2c254399e0387f840e064077a2179b25c3872c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53c7e0f11cd35c2bcc250a79b104cb4d7ad36233")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI_2.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI_2.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
